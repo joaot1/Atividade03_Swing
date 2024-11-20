e82dd8e598092c2417ea240c95e4368913b2d699
joaot1 <joaotombinibortolotti@Gmail.com>
2024-11-20 14:27:47 -0300

    Initial commit

---
+++ src/CadastroDeContatos.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class CadastroDeContas {
+    private JTextField txtNome;
+    private JTextField txtTelefone;
+    private JTextField txtEmail;
+    private JButton btnAdicionar;
+    private JList listaContatos;
+    private JButton btnRemoverContato;
+    private JPanel painelPrincipal;
+    private JLabel lblMensagem;
+}
